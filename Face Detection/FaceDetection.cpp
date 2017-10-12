8acd9a156b6f5e0505de0e170593cfdddce3b772
Sagnik <sagniklp@gmail.com>
2017-10-12 01:42:45 -0400

    fixed left profile, faster

--- Face Detection/FaceDetection.cpp
+++ Face Detection/FaceDetection.cpp
@@ -22,45 +22,22 @@ string face_classifier_dir = "C:\\openCV\\Build\\install\\etc\\haarcascades\\haa
 string prof_face_classifier_dir = "C:\\openCV\\Build\\install\\etc\\haarcascades\\haarcascade_profileface.xml";
 
 
-/*class helper {
-public:
-	CClassf(Mat img= Mat(1,1,CV_64F,cvScalar(0.)), vector<Rect> face_rect(), double scalefactor=1.1, int min_neighbors=3, int flags=0, cv::Size minsize=Size(30,30)) {
-		im = img;
-		faces=face_rect;
-		scale = scalefactor;
-		min_n=min_neighbors;
-		f = flags;
-		minsz = minsize;
-	}
 
-	static vector<Rect> classifiercaller() {
-		return face_classf.detectMultiScale(im, faces, scale, min_n, f, minsz);
-	}
-
-private:
-	Mat im;
+void facecf(Mat img, promise<vector<Rect>> face_rect, double scalefactor, int min_neighbors, int flags, cv::Size minsize, cv::Size maxsize) {
 	vector<Rect> faces;
-	double scale;
-	int min_n;
-	int f;
-	Size minsz;
-};*/
-
-void facecf(Mat img, promise<vector<Rect>> face_rect, double scalefactor, int min_neighbors, int flags, cv::Size minsize) {
-	vector<Rect> faces;
-	face_classf.detectMultiScale(img, faces, scalefactor, min_neighbors, flags, minsize);
+	face_classf.detectMultiScale(img, faces, scalefactor, min_neighbors, flags, minsize, maxsize);
 	//cout << "\n fromf" << faces.size();
 	face_rect.set_value(faces);
 }
-void rpfacecf(Mat img, promise<vector<Rect>> face_rect, double scalefactor, int min_neighbors, int flags, cv::Size minsize) {
+void rpfacecf(Mat img, promise<vector<Rect>> face_rect, double scalefactor, int min_neighbors, int flags, cv::Size minsize, cv::Size maxsize) {
 	vector<Rect> rfaces;
-	rprofile_face_classf.detectMultiScale(img, rfaces, scalefactor, min_neighbors, flags, minsize);
+	rprofile_face_classf.detectMultiScale(img, rfaces, scalefactor, min_neighbors, flags, minsize, maxsize);
 	//cout << "\n fromrpf" << rfaces.size();
 	face_rect.set_value(rfaces);
 }
-void lpfacecf(Mat img, promise<vector<Rect>> face_rect, double scalefactor, int min_neighbors, int flags, cv::Size minsize) {
+void lpfacecf(Mat img, promise<vector<Rect>> face_rect, double scalefactor, int min_neighbors, int flags, cv::Size minsize, cv::Size maxsize) {
 	vector<Rect> lfaces;
-	lprofile_face_classf.detectMultiScale(img, lfaces, scalefactor, min_neighbors, flags, minsize);
+	lprofile_face_classf.detectMultiScale(img, lfaces, scalefactor, min_neighbors, flags, minsize, maxsize);
 	//cout << "\n fromlpf" << lfaces.size();
 	face_rect.set_value(lfaces);
 }
@@ -79,11 +56,11 @@ void facedetect_boundingbox(Mat frame) {
 	Mat flipped_gray_frame, small_flipped_gray_frame;
 	cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
 	flip(gray_frame, flipped_gray_frame, 1);
-	resize(gray_frame, small_gray_frame, Size(), 0.5, 0.5);
-	resize(flipped_gray_frame, small_flipped_gray_frame, Size(), 0.5, 0.5);
-	thread t1(&facecf, small_gray_frame, move(faces), 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(50, 50));
-	thread t2(&rpfacecf, small_flipped_gray_frame, move(right_profile_faces), 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(50, 50));
-	thread t3(&lpfacecf, small_gray_frame, move(left_profile_faces), 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(50, 50));
+	resize(gray_frame, small_gray_frame, Size(), 0.33, 0.33);
+	resize(flipped_gray_frame, small_flipped_gray_frame, Size(), 0.33, 0.33);
+	thread t1(&facecf, small_gray_frame, move(faces), 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(33, 33), Size(80, 80));
+	thread t2(&rpfacecf, small_flipped_gray_frame, move(right_profile_faces), 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(33, 33), Size(80, 80));
+	thread t3(&lpfacecf, small_gray_frame, move(left_profile_faces), 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(33, 33), Size(80, 80));
 	vector<Rect> f = ftr_faces.get();
 	t1.detach();
 	vector<Rect> lf = ftr_left_profile_faces.get(); 
@@ -91,12 +68,12 @@ void facedetect_boundingbox(Mat frame) {
 	vector<Rect> rf = ftr_right_profile_faces.get();
 	t3.detach();
 	
-	int scalef = 2;
+	int scalef = 3;
 
 	if (f.size()) {
 		for (size_t i = 0; i < f.size(); i++)
 		{
-			rectangle(frame,Point(scalef * f[i].x, scalef * f[i].y),Point(scalef * (f[i].x + f[i].width), scalef * (f[i].y + f[i].height)), Scalar(255, 0, 255), 1);
+			rectangle(frame,Point(scalef * f[i].x, scalef * f[i].y),Point(scalef * (f[i].x + f[i].width), scalef * (f[i].y + f[i].height)), Scalar(255, 0, 255), 3);
 		}
 	}
 	else if (lf.size()) {
@@ -108,7 +85,11 @@ void facedetect_boundingbox(Mat frame) {
 	else if (rf.size()) {
 		for (size_t i = 0; i < rf.size(); i++)
 		{
-			rectangle(frame, Point(scalef * rf[i].x, scalef * rf[i].y), Point(scalef * (rf[i].x + rf[i].width), scalef * (rf[i].y + rf[i].height)), Scalar(255, 255, 0), 3);
+			int curr_x = scalef * rf[i].x;
+			int curr_y = scalef * rf[i].y;
+			int flippd_x = frame.cols - curr_x - (rf[i].width*scalef);
+			int flippd_y = curr_y;
+			rectangle(frame, Point(flippd_x, flippd_y), Point(frame.cols - curr_x, curr_y + (scalef * rf[i].height)), Scalar(255, 255, 0), 3);
 		}
 	}
 	//t1.join();
@@ -152,8 +133,8 @@ int main()
 	}
 	capture.release();
 	destroyWindow("Display Window 1");
-	cout << "helo";
-	getchar();
+	//cout << "helo";
+	//getchar();
     return 0;
 }
 
